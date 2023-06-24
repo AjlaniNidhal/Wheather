f5a6e6ba73fa291d13886dd10b0f104d6d23a148
Nidhal <ajlani_nidhal@hotmail.com>
2023-06-24 04:30:32 +0200

    thesource

---
+++ ConverterProject/ConverterService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter.ConverterProject
+{
+    class ConverterService : IDisposable
+    {
+        public double GetFernhaitFromCelcius(double CL)
+        {
+            return (CL * 9.0f / 5.0f) + 32;
+        }
+
+        public double GetCelciusFromFernhait(double FR)
+        {
+            return (FR - 32) * 5.0f / 9.0f;
+        }
+
+        public void Dispose()
+        {
+            Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ ConverterTests/ConverterTest.cs
@@ -0,0 +1,81 @@
+﻿using Converter.ConverterProject;
+using System;
+using Xunit;
+
+namespace Converter.ConverterTests
+{
+    public class ConverterTest : IDisposable
+    {
+        private ConverterService converterService = new ConverterService();
+        //public ConverterTest()
+        //{
+                
+        //}
+        // [Fact]
+        // public void ConvertCilciusToFeranhait_0_32()
+        //{
+        //    //Arrange
+        //    var expected = 32;
+        //    var converterService = new ConverterService();
+
+        //    //Act
+        //    var actual = converterService.GetFernhaitFromCelcius(0);
+
+        //    //Assert
+        //    Assert.Equal(actual, expected);
+
+        //}
+
+        //[Fact]
+        //public void ConvertCilciusToFeranhait_2_1()
+        //{
+        //    //Arrange
+        //    var expected = 1;
+        //    var converterService = new ConverterService();
+
+        //    //Act
+        //    var actual = converterService.GetFernhaitFromCelcius(2);
+
+        //    //Assert
+        //    Assert.Equal(actual, expected);
+
+        //}
+
+        [Theory]
+        [InlineData(0, 32)]
+        [InlineData(-10, 14)]
+        public void ConvertCilciusToFeranhait(double value, double expected)
+        {
+            //Arrange
+            //ConverterService converterService = new ConverterService();
+
+            //Act
+            var actual = converterService.GetFernhaitFromCelcius(value);
+
+            //Assert
+            Assert.Equal(actual, expected);
+
+        }
+
+        [Theory]
+        [InlineData(0, -17.8)]
+        [InlineData(2, -16.7)]
+        public void ConvertFeranhaitToCilcius(double value, double expected)
+        {
+            //Arrange
+            //ConverterService converterService = new ConverterService();
+
+            //Act
+            var actual = converterService.GetCelciusFromFernhait(value);
+
+            //Assert
+            Assert.Equal(actual, expected, 1);
+
+        }
+
+        public void Dispose()
+        {
+            converterService.Dispose();
+        }
+    }
+}
